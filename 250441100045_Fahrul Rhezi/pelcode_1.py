f9323feacfa31b562c99989764a3516d848ea9ee
ejiu1705 <mohzaini17051979@gmail.com>
2025-09-13 14:26:22 +0700

    mengumpulkan tugas

---
+++ 250441100045_Fahrul Rhezi/pelcode_1.py
@@ -0,0 +1,39 @@
+print("="*50)
+print("Nama : Fahrul Rhezi")
+print("="*50)
+nama = "Kenalin, ini Eji"
+print(nama)
+print(type(nama))
+boolean = True
+print(type(boolean))
+
+penjumlahan = 3+4
+print("Hasilnya adalah",penjumlahan)
+
+
+nama_asprak = "Andika"
+nama_callon_dosen = nama_asprak
+print(nama_callon_dosen)
+
+# tipe data number
+# tahun_lahir = int(input("masukkan tahun lahir :"))
+# tahun_sekarang = int(input("masukkan tahun sekarang :"))
+# penghitungan_umur = tahun_sekarang - tahun_lahir
+# print("Umur saya sekarang adalah",penghitungan_umur)
+
+# bilangan_1= 51
+# bilangan_2 = 7
+
+# jumlah = int(bilangan)+int(bilangan_2)
+# print(jumlah)
+
+# cek = bilangan_1 > bilangan_2
+# print(cek)
+
+# modulus = bilangan_1 % bilangan_2
+# print(modulus)
+
+saldo = float(input("Masukkan saldo awal :"))
+deposit = float(input("Masukkan jumlah deposit :"))
+saldo += deposit
+print("Saldo anda sekarang adalah :",saldo)
